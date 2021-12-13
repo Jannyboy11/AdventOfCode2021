cede36fe7392c9bed626dfdaa36b5bc83dbd5731
Jan Boerman <janboerman95@gmail.com>
2021-12-13 22:36:00 +0100

    consistent naming, and fix undefined variable error

--- src/main/scala/day13/Day13.scala
+++ src/main/scala/day13/Day13.scala
@@ -12,8 +12,8 @@ val paper: Paper = lines.takeWhile(_.nonEmpty).map(row => {
 
 val pattern = """fold along ([xy])=(\d+)""".r("axis", "at")
 val folds: List[Fold] = lines.map[Fold] {
-    case pattern("x", number) => FoldX(number.toInt)
-    case pattern("y", number) => FoldY(number.toInt)
+    case pattern("x", at) => FoldX(at.toInt)
+    case pattern("y", at) => FoldY(at.toInt)
 }.toList
 
 case class Point(x: Int, y: Int)
@@ -24,16 +24,17 @@ type Paper = Set[Point]
 type Fold = FoldX | FoldY
 
 def foldAlongX(at: Int, paper: Paper): Paper = paper.map {
-    case Point(x, y) => if x > at then Point(at - (x - at), y) else p
+    case p@Point(x, y) => if x > at then Point(at - (x - at), y) else p
 }
 
 def foldAlongY(at: Int, paper: Paper): Paper = paper.map {
-    case Point(x, y) => if y > at then Point(x, at - (y - at)) else p
+    case p@Point(x, y) => if y > at then Point(x, at - (y - at)) else p
 }
 
-def go(paper: Paper, fold: Fold): Paper = fold match
+def go(paper: Paper, fold: Fold): Paper = fold match {
     case FoldX(at) => foldAlongX(at, paper)
     case FoldY(at) => foldAlongY(at, paper)
+}
 
 @main def main: Unit = {
 
@@ -44,7 +45,7 @@ def go(paper: Paper, fold: Fold): Paper = fold match
     val (maxX, maxY) = foldedPaper.foldLeft((0, 0)) { case ((mX, mY), Point(x, y)) => (Math.max(mX, x), Math.max(mY, y)) }
     for (y <- 0 to maxY) {
         for (x <- 0 to maxX)
-            if foldedPaper.contains(Point(x, y)) then print("##") else print("  ")
+            if foldedPaper(Point(x, y)) then print("##") else print("  ")
         println()
     }
     //val result2 = "ARHZPCUH"
